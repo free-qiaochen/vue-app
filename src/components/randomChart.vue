0b2ccb46d06695dc0057b2d0e030ab3902870eac
陈桥格 <qiaoge.chen@ikang.com>
2020-09-28 16:18:46 +0800

    add chart.js

---
+++ src/components/randomChart.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="small">
+    <line-chart :chart-data="datacollection"></line-chart>
+    <button @click="fillData()">Randomize</button>
+  </div>
+</template>
+
+<script>
+import LineChart from './home.js'
+
+export default {
+  components: {
+    LineChart,
+  },
+  data() {
+    return {
+      datacollection: {},
+    }
+  },
+  mounted() {
+    this.fillData()
+  },
+  methods: {
+    fillData() {
+      this.datacollection = {
+        labels: ['a', 'b', 'c', 'd'],
+        datasets: [
+          {
+            label: 'Data One',
+            backgroundColor: '#f87979',
+            data: [
+              this.getRandomInt(),
+              this.getRandomInt(),
+              this.getRandomInt(),
+              this.getRandomInt(),
+            ],
+          },
+          {
+            label: 'Data One1',
+            backgroundColor: '#999',
+            data: [5, 30, 20, 25, 50, 60, 70],
+            tension: 0,
+            borderWidth: 2,
+          },
+        ],
+      }
+    },
+    getRandomInt() {
+      return Math.floor(Math.random() * (50 - 5 + 1)) + 5
+    },
+  },
+}
+</script>
+
+<style>
+.small {
+  max-width: 600px;
+  margin: 150px auto;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <demoChart />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import demoChart from '@/components/randomChart.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    HelloWorld,
+    demoChart
   }
 }
 </script>
